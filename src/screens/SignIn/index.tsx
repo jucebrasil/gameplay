04f1c98eacd68100384412094810ba80a37cea1e
Juce Brasil <jucebrasil.ti@gmail.com>
2021-06-23 09:41:11 -0400

    Aulas 1 e 2

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import {
+  View, 
+  Text, 
+  TextInput,
+  Image
+
+} from "react-native";
+
+import { ButtonIcon } from "../../components/ButtonIcon";
+import IllustrationImg from '../../assets/illustration.png';
+import { styles } from "./styles";
+import { useNavigation } from "@react-navigation/native";
+
+
+
+export function SignIn() {
+  const navigation = useNavigation();
+
+  function handleSignIn() {
+    navigation.navigate('Home');
+  
+  }
+   return(
+    <View style={styles.container}>
+
+      <Image 
+      source={IllustrationImg}
+      style={styles.Image}
+      resizeMode="stretch"
+      />
+
+      <View 
+      style={styles.content}>
+
+        <Text 
+        style={styles.title}>
+        Conecte-se {'\n'}
+        e organize {'\n'}
+        suas jogatinas {'\n'}
+        
+        </Text>
+
+        <Text 
+        style={styles.subtitle}>
+        Crie grupos para jogar seus games {'\n'}
+        favoritos com seus amigos
+        </Text>
+
+        <ButtonIcon 
+          title="Entrar com Discord"
+           onPress={handleSignIn}
+        
+        
+        />
+      </View>
+    </View>
+
+  );
+}
