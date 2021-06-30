2c9f9e214386b606530e897ecf3016af67a59713
Juce Brasil <jucebrasil.ti@gail.com>
2021-06-29 22:22:11 -0400

    Aula 3

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { View } from 'react-native';
+import { BorderlessButton } from 'react-native-gesture-handler';
+import { Fontisto } from '@expo/vector-icons';
 
 import { Background } from "../../components/Background";
 import { Header } from "../../components/Header";
+import { theme } from '../../global/styles/theme';
 
 //import { Container } from './styles';
 
@@ -10,7 +13,17 @@ export function AppointmentDetails(){
   return (
     <Background>
       <Header
-      title="Detalhes"
+        title="Detalhes"
+        action={
+          <BorderlessButton>
+            <Fontisto
+              name="share"
+              size={24}
+              color={theme.colors.primary}
+            /> 
+
+          </BorderlessButton>
+        }
       
       /> 
     </Background>
